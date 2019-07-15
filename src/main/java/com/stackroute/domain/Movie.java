3270eb461e87c5ba76c54011bb8cca16b7481328
sangrov1 <sangrov1@in.ibm.com>
2019-07-15 12:58:54 +0530

    Task 4

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,28 +1,42 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
     @Autowired
     private Actor actorDiljit;
 
     public Movie(){
-        System.out.println("Injected through constructor");
+
     }
 
     public Movie(Actor actorDiljit) {
-
         this.actorDiljit = actorDiljit;
     }
 
-//    @Autowired
-//    public void setActorDiljit(Actor actorDiljit) {
-//        System.out.println("Injected through setter");
-//        this.actorDiljit = actorDiljit;
-//    }
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean Factory : " + beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Bean Name : " + s);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("ApplicationContext : " + applicationContext);
+    }
 
     @Override
     public String toString() {
