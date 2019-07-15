a5ad093e559c4f43b09a6ea8666dc2d7a57c1a09
sangrov1 <sangrov1@in.ibm.com>
2019-07-15 11:09:34 +0530

    Task 2 modified

--- src/main/java/com/stackroute/BeansConfig.java
+++ src/main/java/com/stackroute/BeansConfig.java
@@ -10,28 +10,27 @@ import org.springframework.context.annotation.Scope;
 public class BeansConfig {
 
     @Bean
-    public Actor actorDiljit()
-    {
-        Actor actor=new Actor("Diljit Dosanjh","Male",35);
+    public Actor actorDiljit() {
+        Actor actor = new Actor("Diljit Dosanjh","Male",35);
         return actor;
     }
+
     @Bean
-    public Actor actorSargun()
-    {
-        Actor actor=new Actor("Sargun Mehta","female",30);
+    public Actor actorSargun() {
+        Actor actor = new Actor("Sargun Mehta","female",30);
         return actor;
     }
+
     @Bean
-    public Actor actorSangha()
-    {
-        Actor actor=new Actor("Harby Sangha","Male",45);
+    public Actor actorSangha() {
+        Actor actor = new Actor("Harby Sangha","Male",45);
         return actor;
     }
 
     @Bean({"movieA","movieB"})
     @Scope("prototype")
     public Movie movie() {
-        Movie movie =new Movie(actorDiljit());
+        Movie movie = new Movie(actorDiljit());
         return movie;
     }
 }
